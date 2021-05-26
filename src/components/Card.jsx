c60aa963dd5a3706a05474bd678db987ca934f67
A_D <ashishdubey@gmail.com>
2021-05-26 14:17:10 +0530

    Initial Commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import { Box, Grid, CardContent, Typography, Card, makeStyles } from '@material-ui/core';
+import CountUp from 'react-countup';
+
+const useStyle = makeStyles({
+    header: {
+        background: '#F5F5F5',
+        padding: 10,
+        color: "black"
+    }
+});
+
+const CardComponent = ({ cardTitle, value, desc, lastUpdate }) => {
+    const classes = useStyle();
+
+    return (
+        <Grid component={Card} style={{ margin: 20, borderBottom: '10px solid grey' }}>
+            <Box className={classes.header}>
+                <Typography variant="h5" color="textSecondary" >{cardTitle}</Typography>
+            </Box>
+            <CardContent>
+                <Typography variant="h5">
+                    <CountUp start={0} end={value} duration={3} seperator="," />
+                </Typography>
+                <Typography color="textSecondary">{desc}</Typography>
+                <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+
+            </CardContent>
+        </Grid>
+    );
+}
+
+export default CardComponent;
